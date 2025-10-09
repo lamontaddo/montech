f76e008bd1a370a51bf4e9418f67bd054bd6868a
lamont <lamontaddo@gmail.com>
2025-10-09 19:44:07 -0400

    chore: custom domain base + CNAME

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <-- IMPORTANT for custom domain (no /montech/)
+  base: '/',   // custom domain => root
 })
